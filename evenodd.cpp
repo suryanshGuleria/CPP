8f6a27a33618a90784d27b8f1f313c4331710a41
SuryanshGuleria <68892002+suryanshGuleria@users.noreply.github.com>
2021-07-12 01:15:21 +0530

    Create evenodd.cpp
    To check whether a number is even or odd.

---
+++ evenodd.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num;
+    cout<<"enter a number \n";
+    cin>>num;
+    if(num%2==0)
+    cout<<"number is even \n";
+    else
+    cout<<"number is odd \n";
+}
